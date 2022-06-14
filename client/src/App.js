64bfc87531f16e87e7f57bfdaac1170a16bba276
Soumya <tatai.9.f@gmail.com>
2022-06-14 21:24:19 +0530

    added nested structure and protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,42 @@
-
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
-import { Dashboard, ErrorPage, LandingPage, RegisterPage } from "./Pages";
-
-
-
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ErrorPage, LandingPage, RegisterPage } from "./Pages";
+import {
+  Stats,
+  SharedLayout,
+  AddJob,
+  Profile,
+  AllJobs,
+} from "./Pages/Dashboard/";
+import { ProtectedRoutes } from "./Pages/ProtectedRoute";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-    <nav>
-      <Link to='/'>Dashboard</Link>
-      <Link to='/landing'>landing</Link>
-      <Link to='/register'>Register</Link>
-    </nav>
-      <Routes>
-<Route path="/" element={<Dashboard/>}></Route>
-<Route path="/register" element={<RegisterPage/>}></Route>
-<Route path="/landing" element={<LandingPage/>}></Route>
-<Route path="*" element={<ErrorPage/>}></Route>
-   
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Dashboard</Link>
+          <Link to="/landing">landing</Link>
+          <Link to="/register">Register</Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoutes>
+                <SharedLayout />
+              </ProtectedRoutes>
+            }
+          >
+            <Route index element={<Stats />} />
+            <Route path="all-jobs" element={<AllJobs />} />
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route path="/landing" element={<LandingPage />}></Route>
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
