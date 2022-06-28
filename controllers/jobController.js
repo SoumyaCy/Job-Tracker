371203f7f57285f4e77b39d90defa53e451a508f
Soumya <tatai.9.f@gmail.com>
2022-06-28 20:43:00 +0530

    complete pagination server setup

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -53,10 +53,17 @@ const getJobs = async (req, res) => {
       break;
   }
 
+  //PAGINATION
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  const totalJobs = await Jobs.countDocuments(queryObject);
+  const noOfPages = Math.ceil(totalJobs / limit);
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, noOfPages: 1 });
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, noOfPages });
 };
 const showStats = async (req, res) => {
   let stats = await Jobs.aggregate([
